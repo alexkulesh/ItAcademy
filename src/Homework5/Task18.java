1fe595411cd7ab7ed1d5b831412764e492fa371d
alexkulesh <57452353+alexkulesh@users.noreply.github.com>
2020-02-29 18:48:14 +0300

    Update Task18.java

--- src/Homework5/Task18.java
+++ src/Homework5/Task18.java
@@ -2,16 +2,17 @@ package Homework5;
 
 /**
  * Task 18. You have the string with text. Find all punctuation marks and count them.
+ *
  */
 public class Task18 {
 
     public static void main(String[] args) {
-        String string = "This sentence has . , - : ; ? !";
+        String text = "This sentence has . , - : ; ? !";
         String symbols = ".,-;:?!";
         int count = 0;
-        for (int i = 0; i < string.length(); i++) {
+        for (int i = 0; i < text.length(); i++) {
             for (int j = 0; j < symbols.length(); j++) {
-                if (string.charAt(i) == symbols.charAt(j)) {
+                if (text.charAt(i) == symbols.charAt(j)) {//Compares symbols of one string to another.
                     count++;
                     break;
                 }
