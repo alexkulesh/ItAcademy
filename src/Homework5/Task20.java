72b64cd8bb97a5cf6a982b85507e2973659b1978
alexkulesh <57452353+alexkulesh@users.noreply.github.com>
2020-02-29 18:48:32 +0300

    Update Task20.java

--- src/Homework5/Task20.java
+++ src/Homework5/Task20.java
@@ -8,23 +8,23 @@ package Homework5;
 public class Task20 {
 
     public static void main(String[] args) {
-        String str = "String with text";
-        char symbol;
-        char letter;
-        for (int i = 0; i < str.length(); i++) {
-            symbol = str.charAt(i);
-            if (i == str.length() - 1) {
-                letter = symbol;
-                System.out.print(letter);
-                break;
-            }
-            if (symbol >= 'A' && symbol <= 'z') {
-                if (str.charAt(i + 1) == ' ' || str.charAt(i + 1) == '.' || str.charAt(i + 1) == ',') {
-                    letter = symbol;
-                    System.out.print(letter);
+        String text = "String with text./ss";
+        String symbols = " .,-;:?!";
+        char lastLetter;
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) >= 'A' && text.charAt(i) <= 'z') { //If there are letters in that position, then...
+                if (i == text.length() - 1) {                   //Finds out if that is the last symbol in the string.
+                    lastLetter = text.charAt(i);
+                    System.out.print(lastLetter);
+                    break;
+                }
+                for (int j = 0; j < symbols.length(); j++) {    //Finds out if that is the last letter in a word.
+                    if (text.charAt(i + 1) == symbols.charAt(j)) {
+                        lastLetter = text.charAt(i);
+                        System.out.print(lastLetter);
+                    }
                 }
             }
         }
     }
-
 }
