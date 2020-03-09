a8b8a509552dd10086cc7e8a95eb4eebaa4b6234
alexkulesh <57452353+alexkulesh@users.noreply.github.com>
2020-03-10 00:23:01 +0300

    Create Fridge.java

---
+++ src/homework6/task24/Fridge.java
@@ -0,0 +1,46 @@
+package homework6.task24;
+
+public class Fridge extends AppForFoodStoring {
+    private int cameraNumber;
+
+    public Fridge(int storingTemp, String storageVolume, int cameraNumber) {
+        super(storingTemp, storageVolume);
+        this.cameraNumber = cameraNumber;
+    }
+
+    public Fridge(int cameraNumber) {
+        this.cameraNumber = cameraNumber;
+    }
+
+    public int getCameraNumber() {
+        return cameraNumber;
+    }
+
+    public void setCameraNumber(int cameraNumber) {
+        this.cameraNumber = cameraNumber;
+    }
+
+    @Override
+    public void setStoringTemp(int storingTemp) {
+        super.setStoringTemp(storingTemp);
+    }
+
+    @Override
+    public void setStorageVolume(String storageVolume) {
+        super.setStorageVolume(storageVolume);
+    }
+
+    @Override
+    public boolean pluggedIn() {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Fridge{" +
+                "cameraNumber=" + cameraNumber +
+                ", storingTemp=" + storingTemp +
+                ", storageVolume='" + storageVolume + '\'' +
+                '}';
+    }
+}
