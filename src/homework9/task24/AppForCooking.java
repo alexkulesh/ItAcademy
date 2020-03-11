f5c15b0cc23c46be7feb4c3d347ac208506ad401
alexkulesh <57452353+alexkulesh@users.noreply.github.com>
2020-03-11 21:03:42 +0300

    Create AppForCooking.java

---
+++ src/homework9/task24/AppForCooking.java
@@ -0,0 +1,20 @@
+package homework9.task24;
+
+public abstract class AppForCooking implements Appliance {
+    int cookingTemp;
+
+    public AppForCooking(int cookingTemp) {
+        this.cookingTemp = cookingTemp;
+    }
+
+    public AppForCooking() {
+    }
+
+    public int getCookingTemp() {
+        return cookingTemp;
+    }
+
+    public void setCookingTemp(int cookingTemp) {
+        this.cookingTemp = cookingTemp;
+    }
+}
