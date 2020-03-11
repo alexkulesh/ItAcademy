64b5e40c65f2d0805e83a91d3e03608751e324d8
alexkulesh <57452353+alexkulesh@users.noreply.github.com>
2020-03-11 21:03:45 +0300

    Create AppForDrinks.java

---
+++ src/homework9/task24/AppForDrinks.java
@@ -0,0 +1,30 @@
+package homework9.task24;
+
+public abstract class AppForDrinks implements Appliance {
+    int waterVolume;
+    int waterTemperature;
+
+    public AppForDrinks(int waterVolume, int waterTemperature) {
+        this.waterVolume = waterVolume;
+        this.waterTemperature = waterTemperature;
+    }
+
+    public AppForDrinks() {
+    }
+
+    public int getWaterVolume() {
+        return waterVolume;
+    }
+
+    public void setWaterVolume(int waterVolume) {
+        this.waterVolume = waterVolume;
+    }
+
+    public int getWaterTemperature() {
+        return waterTemperature;
+    }
+
+    public void setWaterTemperature(int waterTemperature) {
+        this.waterTemperature = waterTemperature;
+    }
+}
