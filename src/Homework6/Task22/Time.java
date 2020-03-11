eb4a0017910659903d451058e02e948e75b76ec2
alexkulesh <57452353+alexkulesh@users.noreply.github.com>
2020-03-11 21:08:53 +0300

    Update Time.java

--- src/Homework6/Task22/Time.java
+++ src/Homework6/Task22/Time.java
@@ -1,18 +1,21 @@
 package homework6.task22;
 
 
-public class Time{
+public class Time {
     private int hour;
     private int minute;
     private int second;
-    public Time(int sec){
+
+    public Time(int sec) {
         this.second = sec;
     }
-    public Time(int hour1, int min1, int sec1){
+
+    public Time(int hour1, int min1, int sec1) {
         this.hour = hour1;
         this.minute = min1;
         this.second = sec1;
     }
+
     public int getHour() {
         return hour;
     }
@@ -37,20 +40,22 @@ public class Time{
         this.second = second;
     }
 
-    public int returnFullTime(){
-        return second + minute*60 + hour*3600;
+    public int returnFullTime() {
+        return second + minute * 60 + hour * 3600;
     }
-    public static void printTime(Time First){
+
+    public static void printTime(Time First) {
         System.out.println(First.returnFullTime() + " second(-s)");
     }
-    public static void compareTime(Time First, Time Second){
-        if(First.returnFullTime() > Second.returnFullTime()){
+
+    public static void compareTime(Time First, Time Second) {
+        if (First.returnFullTime() > Second.returnFullTime()) {
             System.out.println("First time > Second time");
         }
-        if(First.returnFullTime() == Second.returnFullTime()){
+        if (First.returnFullTime() == Second.returnFullTime()) {
             System.out.println("First time = Second time");
         }
-        if(First.returnFullTime() < Second.returnFullTime()){
+        if (First.returnFullTime() < Second.returnFullTime()) {
             System.out.println("Second time > First time");
         }
     }
