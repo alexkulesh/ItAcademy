4f86d3e867213b8afeca9f7529d6ce371b79ec04
alexkulesh <57452353+alexkulesh@users.noreply.github.com>
2020-03-14 17:06:48 +0300

    Create Task26.java

---
+++ src/homework10/task26/Task26.java
@@ -0,0 +1,28 @@
+package homework10.task26;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Task 26. Create list of pupils using ArrayList, fill it up with random
+ * grades. Delete all poor grades out of the list.
+ */
+public class Task26 {
+    public static void main(String[] args) {
+        List<Integer> listOfPupils = new ArrayList<>(10);
+        Random random = new Random();
+        for(int i = 1; i <= 10; i++){
+            listOfPupils.add(random.nextInt(10));
+        }
+        List<Integer> goodGrades = getList(listOfPupils);
+        System.out.println(listOfPupils.toString());
+        System.out.println(goodGrades);
+    }
+    private static List<Integer> getList(List<Integer> goodGrades){
+        final List<Integer> newList = new ArrayList<>(goodGrades);
+        newList.removeIf(i -> i < 4);
+        return newList;
+    }
+}
