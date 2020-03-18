877c7596c6e833c1827c7a1b364cba250ab372fb
alexkulesh <57452353+alexkulesh@users.noreply.github.com>
2020-03-18 22:27:54 +0300

    28 done

---
+++ src/main/java/homework11/Task28/Task28.java
@@ -0,0 +1,28 @@
+package main.java.homework11.Task28;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+/**
+ * Task 28. Create list of pupils grades using ArrayList, fill it up with
+ * random grades. Find the highest grade in the class using iterator.
+ */
+public class Task28 {
+    public static void main(String[] args) {
+        List<Integer> listOfPupils = new Random()
+                .ints(10, 0, 10)
+                .boxed()
+                .collect(Collectors.toList());
+        System.out.println(listOfPupils.toString());
+        ListIterator<Integer> iterator = listOfPupils.listIterator();
+        Integer maxElem = listOfPupils.get(0);
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            if (listOfPupils.get(element) > maxElem) {
+                maxElem = listOfPupils.get(element);
+            }
+        }
+        System.out.println("The highest grade in the class is - " + maxElem);
+    }
+}
